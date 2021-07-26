deb430f4783f96fa170d5c505cb69dd67afe5ba3
Daniel J <orange432@mailbox.org>
2021-07-26 19:32:32 +1000

    swapped register/login form to useReducer instead of useState

--- src/components/login-register.jsx
+++ src/components/login-register.jsx
@@ -1,23 +1,53 @@
-import React,{ useState } from 'react'
+import React,{ useReducer } from 'react'
 import './login-register.scss';
 
+const reducer = (state,action) => {
+  switch(action.type){
+    case "field":
+      return {
+        ...state,
+        [action.field]: action.value
+      }
+    
+    case "loginActive":
+      return {
+        ...state,
+        loginActive: true
+      }
+    
+    case "registerActive":
+      return {
+        ...state,
+        loginActive: false
+      }
+    
+    default:
+      break;
+  }
+}
+
+const initialState = {
+  username: '',
+  password: '',
+  verifyPassword: '',
+  message: '',
+  loginActive: true
+}
+
 const LoginRegister = () => {
-  const [loginActive,setLoginActive] = useState(true);
-  const [username,setUsername] = useState('');
-  const [password,setPassword] = useState('');
-  const [verifyPassword,setVerifyPassword] = useState('');
-  const [message,setMessage] = useState('')
+  const [state, dispatch] = useReducer(reducer,initialState)
+
 
   const msg = text => {
-    setMessage(text);
-    setTimeout(()=>setMessage(''),5000);
+    dispatch({type: "field", field: "message",value: text});
+    setTimeout(()=>dispatch({type: "field", field: "message",value: ''}),5000);
   }
 
   const Login = e => {
     e.preventDefault();
     const query = `
     query{
-      Login(input:{username:"${username}",password:"${password}"}){
+      Login(input:{username:"${state.username}",password:"${state.password}"}){
         success
         session
         message
@@ -44,9 +74,13 @@ const LoginRegister = () => {
 
   const Register = e => {
     e.preventDefault();
+    if(state.password!==state.verifyPassword){
+      msg("Passwords do not match.")
+      return '';
+    }
     const query = `
     mutation{
-      Register(input:{username:"${username}",password:"${password}"}){
+      Register(input:{username:"${state.username}",password:"${state.password}"}){
         success
         code
         message
@@ -64,28 +98,28 @@ const LoginRegister = () => {
     .then(({data})=>{
       if(data.Register.success){
         msg("Registration Successful!");
-        setLoginActive(true);
+        dispatch({type: "loginActive"})
       }else{
         msg(data.Register.message);
       }
     })
   }
 
-  if(loginActive){
+  if(state.loginActive){
     /* Login Form */
     return (
       <div className="login-register">
         <div className="login-register__tabs">
           <div className="login-register__tab active">Login</div>
-          <div className="login-register__tab" onClick={()=>setLoginActive(false)}>Register</div>
+          <div className="login-register__tab" onClick={()=>dispatch({type: "registerActive"})}>Register</div>
         </div>
         <h2 className="login-register__title">Login</h2>
-        <p className="text-center">{message}</p>
+        <p className="text-center">{state.message}</p>
         <form className="login-register__form" onSubmit={Login}>
           <label className="label">Username</label>
-          <input className="input" type="text" minLength="5" pattern="[A-Za-z0-9]{5,}" title="Username (5+ characters, letters and numbers only)" onChange={e=>setUsername(e.target.value)} value={username}/>
+          <input className="input" type="text" minLength="5" pattern="[A-Za-z0-9]{5,}" title="Username (5+ characters, letters and numbers only)" onChange={e=>dispatch({type: "field",value: e.target.value,field: "username"})} value={state.username}/>
           <label className="label">Password</label>
-          <input className="input" type="password" minLength="5" title="Password (5+ characters)" onChange={e=>setPassword(e.target.value)} value={password}/>
+          <input className="input" type="password" minLength="5" title="Password (5+ characters)" onChange={e=>dispatch({type: "field",value: e.target.value,field: "password"})} value={state.password}/>
           <div className="text-center mt-2">
             <button className="btn-primary" type="submit">Login</button>
           </div>
@@ -98,18 +132,18 @@ const LoginRegister = () => {
   return(
     <div className="login-register">
         <div className="login-register__tabs">
-          <div className="login-register__tab" onClick={()=>setLoginActive(true)}>Login</div>
+          <div className="login-register__tab" onClick={()=>dispatch({type: "loginActive"})}>Login</div>
           <div className="login-register__tab active">Register</div>
         </div>
         <h2 className="login-register__title">Register</h2>
-        <p className="text-center">{message}</p>
+        <p className="text-center">{state.message}</p>
         <form className="login-register__form" onSubmit={Register}>
           <label className="label">Username</label>
-          <input className="input" type="text" minLength="5" pattern="[A-Za-z0-9]{5,}" title="Username (5+ characters, letters and numbers only)" onChange={e=>setUsername(e.target.value)} value={username}/>
+          <input className="input" type="text" minLength="5" pattern="[A-Za-z0-9]{5,}" title="Username (5+ characters, letters and numbers only)" onChange={e=>dispatch({action: "field",value: e.target.value,field: "username"})} value={state.username}/>
           <label className="label">Password</label>
-          <input className="input" type="password" minLength="5" title="Password (5+ characters)" onChange={e=>setPassword(e.target.value)} value={password}/>
+          <input className="input" type="password" minLength="5" title="Password (5+ characters)" onChange={e=>dispatch({action: "field",value: e.target.value,field: "password"})} value={state.password}/>
           <label className="label">Verify Password</label>
-          <input className="input" type="password" minLength="5" title="Verify Password (5+ characters)" onChange={e=>setVerifyPassword(e.target.value)} value={verifyPassword}/>
+          <input className="input" type="password" minLength="5" title="Verify Password (5+ characters)" onChange={e=>dispatch({action: "field",value: e.target.value,field: "verifyPassword"})} value={state.verifyPassword}/>
           <div className="text-center">
             <button className="btn-primary" type="submit">Login</button>
           </div>
